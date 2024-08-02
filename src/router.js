360f2a93a3dfc693ff3398bc90deae0ce4a1fbbf
andreialvesreal <andreialvesdev@gmail.com>
2024-08-02 17:43:42 -0300

    Institucional teste

--- src/router.js
+++ src/router.js
@@ -14,7 +14,8 @@ import AsyncStaticScreen from '@screens/auth/async';
 import AuthLoginScreen from '@screens/auth/login';
 import AuthRegisterScreen from '@screens/auth/register';
 
-
+//INSTITUCIONAL
+import InstitucionalScreen from '@screens/institucional';
 
 //TABS SCREEN
 import HomeScreen from '@screens/tabs/home';
@@ -28,72 +29,75 @@ import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 export default function Router() {
   return (
     <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false,}} initialRouteName='AuthRegister'>
+      <Stack.Navigator screenOptions={{ headerShown: false, }} initialRouteName='Institucional'>
 
-            <Stack.Screen name="AuthLogin" component={AuthLoginScreen} options={{...TransitionPresets.SlideFromRightIOS , }}/>
-            <Stack.Screen name="AuthRegister" component={AuthRegisterScreen} options={{...TransitionPresets.SlideFromRightIOS   , }}/>
-          
-            <Stack.Screen name="Async" component={AsyncStaticScreen} options={{...TransitionPresets.RevealFromBottomAndroid   , }}/>
-            <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{...TransitionPresets.SlideFromRightIOS  , }}/>
+        <Stack.Screen name="Institucional" component={InstitucionalScreen} options={{ ...TransitionPresets.SlideFromRightIOS, }} />
 
-            <Stack.Screen name="Tabs" component={Tabs} options={{...TransitionPresets.SlideFromRightIOS , backBehavior: 'none',}}/>
-        </Stack.Navigator>
+        <Stack.Screen name="AuthLogin" component={AuthLoginScreen} options={{ ...TransitionPresets.SlideFromRightIOS, }} />
+        <Stack.Screen name="AuthRegister" component={AuthRegisterScreen} options={{ ...TransitionPresets.SlideFromRightIOS, }} />
+
+
+        <Stack.Screen name="Async" component={AsyncStaticScreen} options={{ ...TransitionPresets.RevealFromBottomAndroid, }} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ ...TransitionPresets.SlideFromRightIOS, }} />
+
+        <Stack.Screen name="Tabs" component={Tabs} options={{ ...TransitionPresets.SlideFromRightIOS, backBehavior: 'none', }} />
+      </Stack.Navigator>
     </NavigationContainer>
-   );
-  }
+  );
+}
 
-function Tabs (){
-    const route = useRoute();
-    const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home'
-      return (
-          <Tab.Navigator initialRouteName="Home" 
-              screenOptions={{
-                headerShown: false,
-                backBehavior: 'none',
-                tabBarActiveTintColor: '#FF26BD',
-                tabBarInactiveTintColor: '#5C0D4580',
-                tabBarStyle: {
-                  backgroundColor: '#FFF',
-                  paddingBottom: 12,
-                  paddingTop: 8,
-                  borderTopWidth: 2,
-                  borderColor: '#F1F1F1',
-                  height: 68,
-                  elevation: 0,
-                },
-              }}
-              >
-              <Tab.Screen name="Home" component={HomeScreen} options={{
-              tabBarLabel: 'Início',
-              tabBarLabelStyle: {fontFamily: routeName === 'Home' ? 'Font_Bold' : 'Font_Book',},
-              tabBarIcon: ({ color, size }) => (<Octicons name="home" size={routeName === 'Home' ? size+3 : size} color={color} />),
-          }}/>
-           
-             <Tab.Screen name="Search" component={SearchScreen} options={{
-              tabBarLabel: 'Buscar',
-              tabBarLabelStyle: {fontFamily: routeName === 'Search' ? 'Font_Bold' : 'Font_Book',},
-              tabBarIcon: ({ color, size }) => (<Octicons name="search" size={routeName === 'Seach' ? size+3 : size} color={color} />),
-          }}/>
-         
-           <Tab.Screen name="Account" component={AccountScreen} options={{
-              tabBarLabel: 'Conta',
-              tabBarLabelStyle: {fontFamily: routeName === 'Account' ? 'Font_Bold' : 'Font_Book',},
-              tabBarIcon: ({ color, size }) => (<FontAwesome6 name="user" size={routeName === 'Account' ? size+3 : size} color={color} />),
-          }}/>
-          </Tab.Navigator>
-      )
-  }
+function Tabs() {
+  const route = useRoute();
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home'
+  return (
+    <Tab.Navigator initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        backBehavior: 'none',
+        tabBarActiveTintColor: '#FF26BD',
+        tabBarInactiveTintColor: '#5C0D4580',
+        tabBarStyle: {
+          backgroundColor: '#FFF',
+          paddingBottom: 12,
+          paddingTop: 8,
+          borderTopWidth: 2,
+          borderColor: '#F1F1F1',
+          height: 68,
+          elevation: 0,
+        },
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarLabel: 'Início',
+        tabBarLabelStyle: { fontFamily: routeName === 'Home' ? 'Font_Bold' : 'Font_Book', },
+        tabBarIcon: ({ color, size }) => (<Octicons name="home" size={routeName === 'Home' ? size + 3 : size} color={color} />),
+      }} />
 
-  /*
-   Transitions só substituir 
-      ModalSlideFromBottomIOS
-      SlideFromRightIOS
-      SlideFromRightIOS
-      FadeFromBottomAndroid 
-      RevealFromBottomAndroid
-      ScaleFromCenterAndroid 
-      DefaultTransition 
-      ModalTransition
+      <Tab.Screen name="Search" component={SearchScreen} options={{
+        tabBarLabel: 'Buscar',
+        tabBarLabelStyle: { fontFamily: routeName === 'Search' ? 'Font_Bold' : 'Font_Book', },
+        tabBarIcon: ({ color, size }) => (<Octicons name="search" size={routeName === 'Seach' ? size + 3 : size} color={color} />),
+      }} />
+
+      <Tab.Screen name="Account" component={AccountScreen} options={{
+        tabBarLabel: 'Conta',
+        tabBarLabelStyle: { fontFamily: routeName === 'Account' ? 'Font_Bold' : 'Font_Book', },
+        tabBarIcon: ({ color, size }) => (<FontAwesome6 name="user" size={routeName === 'Account' ? size + 3 : size} color={color} />),
+      }} />
+    </Tab.Navigator>
+  )
+}
+
+/*
+ Transitions só substituir 
+    ModalSlideFromBottomIOS
+    SlideFromRightIOS
+    SlideFromRightIOS
+    FadeFromBottomAndroid 
+    RevealFromBottomAndroid
+    ScaleFromCenterAndroid 
+    DefaultTransition 
+    ModalTransition
 
 
-  */
+*/
