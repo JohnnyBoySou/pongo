a26ac133bca1fbd5c88b0c50db439d8fe6d61ddb
JohnnyBoySou <joaodesousa101@gmail.com>
2024-08-14 10:47:54 -0300

    new handle

--- src/screens/school/register.js
+++ src/screens/school/register.js
@@ -6,6 +6,9 @@ import PlanosList from '@components/Planos';
 
 export default function SchoolRegisterScreen({ navigation, }) {
     const { color, font, } = useTheme();
+    const handleRegister = (item) => {
+        navigation.navigate('SchoolRegister', { item: item })
+    }
     return (
         <Main>
             <Scroll>
@@ -14,7 +17,7 @@ export default function SchoolRegisterScreen({ navigation, }) {
                     <Column style={{ height: 20, }}></Column>
                     <Title align="center">Cadastrar na escola</Title>
                     <Title size={18}>Selecione o plano</Title>
-                    <PlanosList destino={() => {navigation.navigate('SchoolRegister')}}/>
+                    <PlanosList destino={handleRegister} />
                 </Column>
             </Scroll>
         </Main>
