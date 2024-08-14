3dd579dfedea9ec0e60f57f64b52eb852bdb94e2
andreialvesreal <andreialvesdev@gmail.com>
2024-08-14 08:02:43 -0300

    Serviços Pages

---
+++ src/screens/servicos/data.js
@@ -0,0 +1,20 @@
+export default {
+    data: [
+      {
+        title: '12/06/2024 12:00:00',
+        body:
+          'Pedido criado, aguardando pagamento.',
+      },
+      {
+        title: '12/06/2024 13:23:00',
+        body:
+          'Pedido criado, aguardando pagamento.',
+      },
+      {
+        title: '12/06/2024 14:50:00',
+        body:
+          'Pedido criado, aguardando pagamento.',
+      },
+      
+    ],
+  };
