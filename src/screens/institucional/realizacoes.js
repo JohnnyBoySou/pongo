f8befe046ee0bb31e7952fcd56535d6591d6947c
andreialvesreal <andreialvesdev@gmail.com>
2024-08-20 14:09:23 -0300

    Páginas institucionais

---
+++ src/screens/institucional/realizacoes.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Image, View, Text } from 'react-native';
+import { Main, Scroll, Column, Label, Title, Row, Button, useTheme, } from '@theme/global';
+
+import { MoveRight } from 'lucide-react-native';
+
+//Components
+import Header from '@components/Header';
+import TopMenu from '@components/Header/topmenu';
+import Swiper from 'react-native-swiper';
+import PlanosList from '@components/Planos';
+
+import { useNavigation } from '@react-navigation/native';
+
+import { FlatList } from 'react-native-gesture-handler';
+
+
+export default function RealizacoesScreen() {
+    const navigation = useNavigation();
+    const { color, font, margin } = useTheme();
+    const handleRegister = (item) => {
+        navigation.navigate('SchoolRegister', { item: item })
+    }
+
+    return (
+        <Main style={{ backgroundColor: '#ECEBEB' }}>
+            <Scroll>
+
+
+                <Header title="" />
+
+
+                <Image source={require('@imgs/extensao-da-sua-casa.png')} style={{ width: '100%', position: 'absolute', height: 270, borderBottomLeftRadius: 20, borderBottomRightRadius: 20, zIndex: -1 }} />
+
+                <Column style={{ height: 240 }} />
+
+                <Column mh={margin.h}>
+
+                    <Title style={{ marginBottom: 24 }}>Realizações</Title>
+
+                    <Text style={{ color: '#979797', marginBottom: 12 }}>
+                        Tombado pelo Patrimônio Histórico Nacional, O Copacabana Palace é símbolo do glamour carioca.
+                    </Text>
+
+                    <Text style={{ color: '#979797', marginBottom: 12 }}>
+                        Localizado em frente à Praia de Copacabana, na cidade do Rio de Janeiro o hotel já foi eleito diversas vezes como o melhor hotel da América do Sul e os mais importantes prêmios mundiais de turismo.
+                    </Text>
+
+                    <Text style={{ color: '#979797', marginBottom: 12 }}>
+                        Pet Friendly, o Copa agora oferece nossos produtos para seus hóspedes pets!
+                    </Text>
+                </Column>
+
+
+
+                <Column mh={margin.h} style={{ marginTop: 24, marginBottom: 24 }}>
+                    <FlatList
+                        data={images.slice(0, 4)}
+                        horizontal
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item, index }) => (
+                            <Column style={{ position: 'relative', marginRight: 20 }}>
+                                <Image source={{ uri: item }} style={{ width: 260, height: 300, borderRadius: 12 }} />
+                            </Column>
+                        )}
+                    />
+                </Column>
+
+
+
+            </Scroll>
+        </Main>
+    );
+}
+
+const images = [
+    'https://img.freepik.com/free-photo/top-view-pet-accessories_23-2150930406.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1720483200&semt=ais_hybrid',
+    'https://img.freepik.com/free-photo/top-view-pet-accessories_23-2150930406.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1720483200&semt=ais_hybrid',
+    'https://img.freepik.com/free-photo/top-view-pet-accessories_23-2150930406.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1720483200&semt=ais_hybrid',
+    'https://img.freepik.com/free-photo/top-view-pet-accessories_23-2150930406.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1720483200&semt=ais_hybrid',
+    'https://img.freepik.com/free-photo/top-view-pet-accessories_23-2150930406.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1720483200&semt=ais_hybrid',
+    'https://img.freepik.com/free-photo/top-view-pet-accessories_23-2150930406.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1720483200&semt=ais_hybrid'
+];
