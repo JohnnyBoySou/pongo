42dbeae4c2e851b5638c97001c24585ec7ea39d9
JohnnyBoySou <joaodesousa101@gmail.com>
2024-08-21 13:49:32 -0300

    gallery

--- src/screens/institucional/singe_galeria.js
+++ src/screens/institucional/singe_galeria.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Main, Scroll, Column, Label, Title, Row, Image, useTheme } from '@theme/global';
+import Back from '@components/Back';
 export default function InstitucionalSingleGaleriaScreen({ navigation, route }) {
     const img = route.params?.img
     return (
         <Main>
+            <Back />
             <Image source={{ uri: img, }} style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
         </Main>
     )
