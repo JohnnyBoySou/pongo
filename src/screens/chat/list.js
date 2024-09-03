dac25ef1119df2060daa7146afea741d40f4c7c5
JohnnyBoySou <joaodesousa101@gmail.com>
2024-09-03 17:57:49 -0300

    adjust refresh

--- src/screens/chat/list.js
+++ src/screens/chat/list.js
@@ -1,28 +1,24 @@
 import { Main, Button, Column, Label, Title, Row, Image, useTheme, useNavigate, Loader, LabelBT, Scroll, ButtonPrimary } from '@theme/global';
-import { FlatList } from 'react-native-gesture-handler';
 import TopMenu from '@components/Header/topmenu';
 import TabBar from '@components/TabBar';
 import { useEffect, useState } from 'react';
 import { listChats } from '@api/request/chat';
 import { formatDateTime } from '@hooks/utils';
-import { TextInput } from 'react-native-gesture-handler';
 import { MessageCircleMore, Search } from 'lucide-react-native';
 import Back from '@components/Back';
 import { searchChats, } from '@api/request/chat';
-import { RefreshControl } from 'react-native';
+import { RefreshControl, TouchableOpacity, TextInput, FlatList } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 
-import socket from '@hooks/socket';
-
+//import socket from '@hooks/socket';
 
 export default function ChatListScreen({ navigation }) {
     const { color, font, margin } = useTheme();
-    const [loading, setloading] = useState();
+    const [loading, setloading] = useState(true);
     const [data, setdata] = useState([]);
     const [search, setsearch] = useState();
     const [page, setpage] = useState(1);
     const isFocused = useIsFocused();
-
 
     const fecthData = async () => {
         setloading(true)
@@ -41,11 +37,11 @@ export default function ChatListScreen({ navigation }) {
     }, [isFocused])
 
 
-    useEffect(() => {
-    //    socket.on('chat message', async (dados) => {
+    //useEffect(() => {
+        //    socket.on('chat message', async (dados) => {
         //    setmessages((msgs) => [...msgs, dados])
-     //   });
-    }, [socket]);
+        //   });
+   // }, [socket]);
 
     const handleSearch = async () => {
         if (search?.length > 1) {
@@ -55,7 +51,7 @@ export default function ChatListScreen({ navigation }) {
             } catch (error) {
 
             }
-        } else {   
+        } else {
             const res = await listChats(page, 'U')
             setdata(res)
         }
@@ -63,80 +59,80 @@ export default function ChatListScreen({ navigation }) {
 
     return (
         <Main>
-            <Scroll>
-                <TopMenu cart={false} search={false} back={false} />
-                <Column mh={margin.h} mv={12}>
-                    <Title size={26}>Suas conversas</Title>
-                    <Row style={{ justifyContent: 'center', alignItems: 'center', flex: 1, marginHorizontal: 24, marginTop: 10, }}>
-                        <Back />
-                        <Row style={{ backgroundColor: '#fff', paddingRight: 8, borderRadius: 10, justifyContent: 'center', alignItems: 'center', marginLeft: 12, }}>
-                            <TextInput
-                                value={search}
-                                onChangeText={(e) => setsearch(e)}
-                                placeholder='Pesquisar'
-                                onSubmitEditing={handleSearch}
-                                style={{ paddingHorizontal: 20, paddingVertical: 14, fontSize: 16, fontFamily: font.medium, flex: 1 }}
+            {loading ? <Loader /> :
+                <Column>
+                    <FlatList
+                        data={data}
+                        renderItem={({ item }) => <Chat item={item} />}
+                        keyExtractor={item => item.id}
+                        windowSize={8}
+                        initialNumToRender={8}
+                        maxToRenderPerBatch={8}
+                        refreshing={loading}
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={loading}
+                                onRefresh={fecthData}
+                                colors={[color.sc.sc3]}
                             />
-                            <Button bg={color.title} ph={1} pv={1} onPress={handleSearch} style={{ width: 42, height: 42, justifyContent: 'center', alignItems: 'center', }} radius={8}>
-                                <Search size={18} color="#fff" />
-                            </Button>
-                        </Row>
-                    </Row>
+                        }
+                        ListHeaderComponent={() => <Column>
+                            <TopMenu cart={false} search={false} back={false} />
+                            <Column mh={margin.h} mv={12}>
+                                <Title size={26}>Suas conversas</Title>
+                                <Row style={{ justifyContent: 'center', alignItems: 'center', flex: 1, marginHorizontal: 24, marginTop: 10, }}>
+                                    <Back />
+                                    <Row style={{ backgroundColor: '#fff', paddingRight: 8, borderRadius: 10, justifyContent: 'center', alignItems: 'center', marginLeft: 12, }}>
+                                        <TextInput
+                                            value={search}
+                                            onChangeText={(e) => setsearch(e)}
+                                            placeholder='Pesquisar'
+                                            onSubmitEditing={handleSearch}
+                                            style={{ paddingHorizontal: 20, paddingVertical: 14, fontSize: 16, fontFamily: font.medium, flex: 1 }}
+                                        />
+                                        <Button bg={color.title} ph={1} pv={1} onPress={handleSearch} style={{ width: 42, height: 42, justifyContent: 'center', alignItems: 'center', }} radius={8}>
+                                            <Search size={18} color="#fff" />
+                                        </Button>
+                                    </Row>
+                                </Row>
+                            </Column>
+                        </Column>
+                        }
+                        ListEmptyComponent={() => <Label size={14} style={{ textAlign: 'center', marginVertical: 80, }}>Nenhum atendimento encontrado</Label>}
+                        ItemSeparatorComponent={() => <Column style={{ height: 1, flexGrow: 1, backgroundColor: color.border, marginVertical: 4, borderRadius: 6, }} />}
+                    />
+                    <Button onPress={() => { navigation.navigate('ChatNew') }} style={{ borderWidth: 2, borderColor: '#918C8B', }} pv={16} ph={1} mh={margin.h} mtop={30}>
+                        <LabelBT color="#918C8B" style={{ textAlign: 'center', fontSize: 20 }}> <MessageCircleMore color={color.title} size={20} /> Iniciar conversa</LabelBT>
+                    </Button>
+                    <Column style={{ height: 100, width: 20, }}></Column>
                 </Column>
-
-                {loading ? <Loader /> :
-                    <Column>
-                        <FlatList
-                            data={data}
-                            renderItem={({ item }) => <Chat item={item} />}
-                            keyExtractor={item => item.id}
-                            windowSize={10}
-                            maxToRenderPerBatch={6}
-                            onRefresh={() => {fecthData()}}
-                            refreshing={loading}
-                            refreshControl={
-                                <RefreshControl
-                                    refreshing={loading} 
-                                    onRefresh={fecthData}  
-                                    colors={[color.sc.sc3]}  
-                                />
-                            }
-                            ListEmptyComponent={() => <Label size={14} style={{ textAlign: 'center', marginVertical: 80, }}>Nenhum atendimento encontrado</Label>}
-                            ItemSeparatorComponent={() => <Column style={{ height: 1, flexGrow: 1, backgroundColor: color.border, marginVertical: 4, borderRadius: 6, }} />}
-                        />
-                        <Button onPress={() => { navigation.navigate('ChatNew') }} style={{ borderWidth: 2, borderColor: '#918C8B', }} pv={16} ph={1} mh={margin.h} mtop={30}>
-                            <LabelBT color="#918C8B" style={{ textAlign: 'center', fontSize: 20 }}> <MessageCircleMore color={color.title} size={20} /> Iniciar conversa</LabelBT>
-                        </Button>
-                        <Column style={{ height: 100, width: 20, }}></Column>
-                    </Column>
-                }
-            </Scroll>
+            }
             <TabBar />
         </Main>
     )
 }
 
 const Chat = ({ item, }) => {
-    const { color } = useTheme();
     const { alterado_em, unread, token_chat, criado_em, titulo, avatarcolaborador } = item
     const navigation = useNavigate()
 
 
     const img = avatarcolaborador ? { uri: avatarcolaborador } : require('@imgs/btn-onde-estamos.png')
     return (
-        <Button onPress={() => { navigation.navigate('ChatDetails', { token: token_chat, user: item, type: 'U', }) }} radius={4} ph={28}>
+        <Column pv={12}>
+        <TouchableOpacity onPress={() => { navigation.navigate('ChatDetails', { token: token_chat, user: item, type: 'U', }) }} style={{ paddingHorizontal: 28, paddingVertical: 2,}}>
             <Row style={{ justifyContent: 'space-between', alignItems: 'center', }}>
                 <Image style={{ width: 56, height: 56, borderRadius: 100, backgroundColor: '#f7f7f7', }} source={img} />
                 <Column style={{ flexGrow: 1, marginLeft: 12, }}>
-                    <Title size={18}>{titulo}</Title>
-                    <Label size={14} style={{ marginTop: 4, }}>{formatDateTime(alterado_em).slice(14)}</Label>
+                    <Title size={16}>{titulo}</Title>
+                    <Label size={12} style={{ marginTop: 4, }}>{formatDateTime(alterado_em).slice(14)}</Label>
                 </Column>
                 <Column style={{ alignItems: 'flex-end' }}>
                     <Label size={14}>{formatDateTime(criado_em).slice(0, 10)}</Label>
-                    {unread > 0 && <Column style={{ backgroundColor: color.blue, marginTop: 4, width: 26, height: 26, justifyContent: 'center', borderRadius: 100, alignItems: 'center', }}><Label style={{ color: '#fff', fontFamily: 'Font_Bold', marginTop: 2, fontSize: 14 }}>{unread}</Label></Column>}
                 </Column>
             </Row>
-        </Button>
+        </TouchableOpacity>
+        </Column>
     )
 }
 
--- src/screens/colaborador/list.js
+++ src/screens/colaborador/list.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
-import { Main, Button, Column, Label, Title, Row, Image, useTheme, useNavigate, LabelBT, Scroll, Back } from '@theme/global';
-import { FlatList, TextInput } from 'react-native-gesture-handler';
-import { Search } from 'lucide-react-native';
+import { Main, Button, Column, Label, Title, Row, Image, Scroll, useTheme, useNavigate, LabelBT, Loader, Back } from '@theme/global';
+import { RefreshCcw, Search } from 'lucide-react-native';
 import { listChats, searchChats } from '@api/request/colaborador';
 import { formatDateTime } from '@hooks/utils';
 import { getPreferences } from '@hooks/colaborador';
-import { RefreshControl } from 'react-native';
+import { RefreshControl, TouchableOpacity , FlatList, TextInput } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 
 export default function ChatColaboradorListScreen({ navigation }) {
@@ -47,77 +46,75 @@ export default function ChatColaboradorListScreen({ navigation }) {
     }
     return (
         <Main>
-            <Scroll>
-
-                <Column mh={margin.h} mv={20}>
-                    <Row style={{ justifyContent: 'space-between', alignItems: 'center', }}>
-                        <Title size={32}>Chats </Title>
-                        <Row>
-                            <Column style={{ justifyContent: 'center', }}>
-                                <Title size={16}>{user?.name}</Title>
-                                <Button pv={1} ph={14} style={{ alignSelf: 'flex-end' }} onPress={() => { navigation.navigate('AuthLoginColaborador') }} >
-                                    <LabelBT size={14} color={color.red}>Sair</LabelBT>
-                                </Button>
-                            </Column>
-                            <Image source={{ uri: user?.avatar }} style={{ width: 54, borderRadius: 100, height: 54, objectFit: 'contain', }} />
-                        </Row>
-                    </Row>
-                    <Row style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20, flex: 1, marginHorizontal: 24, }}>
-                        <Back />
-                        <Row style={{ backgroundColor: '#fff', paddingRight: 8, borderRadius: 10, justifyContent: 'center', alignItems: 'center', marginLeft: 12, }}>
-                            <TextInput
-                                value={search}
-                                onChangeText={(e) => setsearch(e)}
-                                placeholder='Pesquisar'
-                                onSubmitEditing={handleSearch}
-                                style={{ paddingHorizontal: 20, paddingVertical: 14, fontSize: 16, fontFamily: font.medium, flex: 1 }}
+            {loading ? <Loader /> :
+                    <FlatList
+                        data={data}
+                        renderItem={({ item }) => <Chat item={item} user={user} />}
+                        keyExtractor={item => item.id}
+                        windowSize={8}
+                        initialNumToRender={8}
+                        maxToRenderPerBatch={8}
+                        refreshing={loading}
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={loading}
+                                onRefresh={fecthData}
+                                colors={[color.sc.sc3]}
                             />
-                            <Button bg={color.title} ph={1} pv={1} onPress={handleSearch} style={{ width: 42, height: 42, justifyContent: 'center', alignItems: 'center', }} radius={8}>
-                                <Search size={18} color="#fff" />
-                            </Button>
-                        </Row>
-                    </Row>
-                </Column>
-
-
-
-                <Column>
-
-                </Column>
-
-                <FlatList
-                    data={data}
-                    renderItem={({ item }) => <Chat item={item} user={user} />}
-                    keyExtractor={item => item.id}
-                    ItemSeparatorComponent={() => <Column style={{ height: 1, flexGrow: 1, backgroundColor: color.border, marginVertical: 4, borderRadius: 6, }} />}
-                    windowSize={10}
-                    onRefresh={() => {fecthData()}}
-                    refreshing={loading}
-                    refreshControl={
-                        <RefreshControl
-                            refreshing={loading}
-                            onRefresh={fecthData}
-                            colors={[color.sc.sc3]}
-                        />
-                    }
-                    maxToRenderPerBatch={6}
-                    ListFooterComponent={() => <Column mv={40}>{data?.length === 20 &&
-                        <Button mv={20} mh={28} onPress={() => { setpage(parseInt(page) + 1) }} bg={color.label} style={{ justifyContent: 'center', alignItems: 'center', }}><LabelBT color="#fff">Mostrar mais chats</LabelBT></Button>}</Column>}
-                    ListEmptyComponent={() => <Label size={14} style={{ textAlign: 'center', marginVertical: 80, }}>Nenhum atendimento encontrado</Label>}
-                />
-            </Scroll>
+                        }
+                        ListHeaderComponent={
+                            <Column>
+                                <Column mh={margin.h} mv={20}>
+                                    <Row style={{ justifyContent: 'space-between', alignItems: 'center', }}>
+                                        <Title size={32}>Chats </Title>
+                                        <Row>
+                                            <Column style={{ justifyContent: 'center', }}>
+                                                <Title size={16}>{user?.name}</Title>
+                                                <Button pv={1} ph={14} style={{ alignSelf: 'flex-end' }} onPress={() => { navigation.navigate('AuthLoginColaborador') }} >
+                                                    <LabelBT size={14} color={color.red}>Sair</LabelBT>
+                                                </Button>
+                                            </Column>
+                                            <Image source={{ uri: user?.avatar }} style={{ width: 54, borderRadius: 100, height: 54, objectFit: 'contain', }} />
+                                        </Row>
+                                    </Row>
+                                    <Row style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20, flex: 1, marginHorizontal: 24, }}>
+                                        <Button onPress={fecthData} pv={1} ph={1} bg="#fff" style={{ width: 42, height: 42, justifyContent: 'center', alignItems: 'center', }}>
+                                            <RefreshCcw size={18} color={color.title} />
+                                        </Button>
+                                        <Row style={{ backgroundColor: '#fff', paddingRight: 8, borderRadius: 10, justifyContent: 'center', alignItems: 'center', marginLeft: 12, }}>
+                                            <TextInput
+                                                value={search}
+                                                onChangeText={(e) => setsearch(e)}
+                                                placeholder='Pesquisar'
+                                                onSubmitEditing={handleSearch}
+                                                style={{ paddingHorizontal: 20, paddingVertical: 14, fontSize: 16, fontFamily: font.medium, flex: 1 }}
+                                            />
+                                            <Button bg={color.title} ph={1} pv={1} onPress={handleSearch} style={{ width: 42, height: 42, justifyContent: 'center', alignItems: 'center', }} radius={8}>
+                                                <Search size={18} color="#fff" />
+                                            </Button>
+                                        </Row>
+                                    </Row>
+                                </Column>
+                            </Column>
+                        }
+                        ListFooterComponent={() => <Column mv={40}>{data?.length === 20 &&
+                            <Button mv={20} mh={28} onPress={() => { setpage(parseInt(page) + 1) }} bg={color.label} style={{ justifyContent: 'center', alignItems: 'center', }}><LabelBT color="#fff">Mostrar mais chats</LabelBT></Button>}</Column>}
+                        ListEmptyComponent={() => <Label size={14} style={{ textAlign: 'center', marginVertical: 80, }}>Nenhum atendimento encontrado</Label>}
+                        ItemSeparatorComponent={() => <Column style={{ height: 1, flexGrow: 1, backgroundColor: color.border, marginVertical: 4, borderRadius: 6, }} />}
+                   />
+            }
         </Main>
     )
 }
 
 
 const Chat = ({ item, }) => {
-    const { color } = useTheme();
     const { avatarcolaborador, unread, token_chat, criado_em, titulo, nomeusuario } = item
     const navigation = useNavigate()
     if (!item) return null
     return (
-        <Button onPress={() => { navigation.navigate('ChatDetails', { token: token_chat, user: item, type: 'C' }) }} radius={4} ph={28}>
+        <Column pv={12}>
+        <TouchableOpacity  onPress={() => { navigation.navigate('ChatDetails', { token: token_chat, user: item, type: 'C' }) }}  style={{ paddingHorizontal: 28, paddingVertical: 2,}}>
             <Row style={{ justifyContent: 'space-between', alignItems: 'center', }}>
                 <Image style={{ width: 56, height: 56, borderRadius: 100, backgroundColor: '#f7f7f7', }} source={{ uri: avatarcolaborador }} />
                 <Column style={{ flexGrow: 1, marginLeft: 12, }}>
@@ -128,7 +125,8 @@ const Chat = ({ item, }) => {
                     <Label size={14}>{formatDateTime(criado_em).slice(0, 10)}</Label>
                 </Column>
             </Row>
-        </Button>
+        </TouchableOpacity>
+        </Column>
     )
 }
 //{unread > 0 && <Column style={{ backgroundColor: color.blue, marginTop: 4, width: 26, height: 26, justifyContent: 'center', borderRadius: 100, alignItems: 'center', }}><Label style={{ color: '#fff', fontFamily: 'Font_Bold', marginTop: 2, fontSize: 14 }}>{unread}</Label></Column>}
